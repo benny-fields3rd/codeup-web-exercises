a2fe0c13dfe67c45263b7e124bc4dee819900156
benny-fields3rd <benny.fields3rd@gmail.com>
2018-10-04 16:42:39 -0500

    added conditionals exercise html and js files

--- js/conditionals.js
+++ js/conditionals.js
@@ -15,7 +15,31 @@
  *
  * Can you refactor your code to use functions?
  */
-
+// var inputNumber = confirm("Would you like to enter a number?");
+//
+// if(inputNumber === true) {
+//     var confirmedNumber = Number(prompt("Great! Please enter a number:"));
+//     var plus100 = confirmedNumber + 100;
+//
+//     if(isNaN(confirmedNumber) === true) {
+//         alert("You did not enter a number!");
+//     } else {
+//         if (confirmedNumber === isNaN) {
+//             alert("You did not enter a number!");
+//         }
+//         if (confirmedNumber % 2 === 1) {
+//             alert("Your number is odd.");
+//         } else {
+//             alert("Your number is even.");
+//         }
+//         alert(confirmedNumber + " plus 100 is: " + plus100);
+//         if (confirmedNumber > 0) {
+//             alert(confirmedNumber + " is also a positive number.");
+//         } else {
+//             alert(confirmedNumber + " is also a negative number.")
+//         }
+//     }
+// }
 /* ########################################################################## */
 
 /**
@@ -35,6 +59,32 @@
  * Test your function by passing various string literals to it and
  * console.logging the function's return value
  */
+var inputColor = prompt("What is your favorite color?");
+
+function analyzeColor(inputColor) {
+    if(inputColor === "blue") {
+        alert("blue is the color of the sky.");
+    } else if(inputColor === "red") {
+        alert("Strawberries are red.");
+    } else if(inputColor === "cyan") {
+        alert("I don't know anything about cyan.");
+    } else if(inputColor === "violet") {
+        alert("Violet cars are not cool.");
+    } else if(inputColor === "yellow") {
+        alert("The sun is supposedly yellowish.");
+    } else if(inputColor === "green") {
+        alert("Grass is sometimes greener on the other side.");
+    } else if(inputColor === "orange") {
+        alert("Oranges are a great fruit...well only the navel ones.");
+    } else if(inputColor === "indigo") {
+        alert("What kind of color is indigo?");
+    } else {
+        alert("You didn't use a known color.");
+    }
+    return inputColor;
+}
+analyzeColor(inputColor);
+
 
 // Don't change the next two lines!
 // These lines create two variables for you:
@@ -48,19 +98,50 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * Pass the `randomColor` variable to your function and console.log the results.
  * You should see a different message everytime you refresh the page
  */
-
+// console.log(analyzeColor(randomColor));
 /**
  * TODO:
  * Refactor your above function to use a switch-case statement
  */
+var colorChoice = prompt("What is your favorite color?");
 
+switch (colorChoice) {
+    case "blue":
+        alert("blue is the color of the sky.");
+        break;
+    case "red":
+        alert("Strawberries are red.");
+        break;
+    case "cyan":
+        alert("I don't know anything about cyan.");
+        break;
+    case "violet":
+        alert("Violet cars are not cool.");
+        break;
+    case "yellow":
+        alert("The sun is supposedly yellowish.");
+        break;
+    case "green":
+        alert("Grass is sometimes greener on the other side.");
+        break;
+    case "orange":
+        alert("Oranges are a great fruit...well only the navel ones.");
+        break;
+    case "indigo":
+        alert("What kind of color is indigo?");
+        break;
+    default:
+        alert("You didn't use a known color.");
+        break;
+}
 /**
  * TODO:
  * Prompt the user for a color when the page loads, and pass the input from the
  * user to your `analyzeColor` function. Alert the return value from your
  * function to show it to the user.
  */
-
+var userColor = prompt("Enter a color please.");
+    alert(analyzeColor(userColor));
 /* ########################################################################## */
 
 /**
