aacedc67909376d6a8539b633347918515a2b3c8
benny-fields3rd <benny.fields3rd@gmail.com>
2018-10-04 09:07:05 -0500

    updated function-bonuses.js file and added function-drills files

--- js/function-bonuses.js
+++ js/function-bonuses.js
@@ -63,50 +63,61 @@ function last(input2) {
 }
 console.log(last("Programmer"));
 // Write a function called `rest(input)` that returns everything but the first character of a string.
-function rest() {
-
+function rest(noFirstString) {
+    var output = noFirstString.slice(1);
+    return output;
 }
+console.log(rest("Codeup"));
 // Write a function called `reverse(input)` that takes a string and returns it reversed.
-function reverse() {
-
+function reverse(str) {
+    return str.split("").reverse().join("");
 }
+console.log(reverse("oprah"));
 // Write a function called `isNumeric(input)` that takes an input and returns a boolean if the input is numeric.
 function isNumeric() {
 
 }
 // Write a function called `count(input)` that takes in a string and returns the number of characters.
-function count() {
+function count(string) {
 
 }
 // Write a function called `add(a, b)` that returns the sum of a and b
 function add(a, b) {
-
+    return a + b;
 }
 // Write a function called `subtract(a, b)` that return the difference between the two inputs.
 function subtract(a, b) {
-
+    return a - b;
 }
 // Write `multiply(a, b)` function that returns the product
 function multiply(a, b) {
-
+    return a * b;
 }
 // Write a divide(numerator, denominator) function that returns a divided by b
 function divide(numerator, denominator) {
-
+    return numerator / denominator;
 }
 // Write a remainder(number, divisor) function that returns the remainder left over when dividing `number` by the `divisor`
 function remainder(number, divisor) {
-
+    return number % divisor;
 }
 // Write the function `square(a)` that takes in a number and returns the number multiplied by itself.
 function square(a) {
-
+    return a * a;
 }
 // # Super Duper Gold-Star Bonus
-// Write a function called sumOfSquares(a, b) that uses only your add() function and your square function and not + or * operators
+// Write a function called sumOfSquares(a, b) that uses only your add() function and your square function
+// and not + or * operators
 function sumOfSquares(a, b) {
-
+    var output;
+    return output;
 }
+console.log(sumOfSquares(add(10, 10)));
+console.log(sumOfSquares())
 // Write a function called doMath(operator, a, b) that takes 3 parameters. The first parameter is the name of the math function you
 // want to apply. a and b are the two numbers to run that function on.
-
+function doMath(operator, a, b) {
+    var output = operator(a, b);
+        return output;
+}
+console.log(doMath(add, 5, 1));
