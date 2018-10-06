69dfa4ce54e92a1d447f19475e8a67218b9003d5
benny-fields3rd <benny.fields3rd@gmail.com>
2018-10-05 20:24:44 -0500

    completed function-drills-simple

--- js/function-drills-simple.js
+++ js/function-drills-simple.js
@@ -47,18 +47,82 @@ function getRandomNumber(min, max) {
 }
 console.log(getRandomNumber());
 // Write a function called `first(input)` that returns the first character in the provided string.
+function first(inputString) {
+    return inputString.charAt(0);
+}
+console.log(first("Robot"));
 // Write a function called `last(input)` that returns the last character of a string
+function last(inputString) {
+    return inputString.charAt(inputString.length-1);
+}
+console.log(last("Robot"));
 // Write a function called `rest(input)` that returns everything but the first character of a string.
+function rest(restInput) {
+    return restInput.slice(1);
+}
+console.log(rest("Codeup"));
 // Write a function called `reverse(input)` that takes a string and returns it reversed.
+function reverse(revInput) {
+    return revInput.split("").reverse().join("");
+}
+console.log(reverse("Oprah"));
 // Write a function called `isNumeric(input)` that takes an input and returns a boolean if the input is numeric.
+function isNumeric(input) {
+    if (input === true) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isNumeric("b"));
 // Write a function called `count(input)` that takes in a string and returns the number of characters.
+function count(input) {
+    return input.length;
+}
+console.log(count("Graduation"));
 // Write a function called `add(a, b)` that returns the sum of a and b
+function add(a, b) {
+    return parseFloat(a + b);
+}
+console.log(add(100, 5));
 // Write a function called `subtract(a, b)` that return the difference between the two inputs.
+function subtract(a, b) {
+    return parseFloat(a - b);
+}
+console.log(subtract(100, 5));
 // Write `multiply(a, b)` function that returns the product
+function multiply(a, b) {
+   return parseFloat(a * b);
+}
+console.log(multiply(10, 10));
 // Write a divide(numerator, denominator) function that returns a divided by b
+function divide(numerator, denominator) {
+    return parseFloat(numerator / denominator);
+}
+console.log(divide(100, 10));
 // Write a remainder(number, divisor) function that returns the remainder left over when dividing `number` by the `divisor`
+function remainder(number, divisor) {
+    return parseFloat(number % divisor);
+}
+console.log(remainder(100, 1));
 // Write the function `square(a)` that takes in a number and returns the number multiplied by itself.
+function square(a) {
+    return parseFloat(a * a);
+}
+console.log(square(8));
 // # Super Duper Gold-Star Bonus
 // Write a function called sumOfSquares(a, b) that uses only your add() function and your square function and not + or * operators
-// Write a function called doMath(operator, a, b) that takes 3 parameters. The first parameter is the name of the math function you want to apply. a and b are the two numbers to run that function on.
+function sumOfSquares(a, b) {
+    a = parseFloat(square(a));
+    b = parseFloat(square(b));
+    return add(a, b);
+}
+console.log(sumOfSquares(5, 5));
+
+// Write a function called doMath(operator, a, b) that takes 3 parameters.
+// The first parameter is the name of the math function you want to apply. a and b are the two numbers to run that function on.
+function doMath(operator, a, b) {
+    return operator;
+}
+console.log(doMath(multiply(6, 6)));
 // })()
