6e3d3a60578b1fefafa0a975eeed1a74dc936d5e
benny-fields3rd <benny.fields3rd@gmail.com>
2018-10-08 19:59:45 -0500

    added loops exercise files

---
+++ js/for_loops.js
@@ -0,0 +1,36 @@
+"use strict";
+/* 2.
+Create a function named showMultiplicationTable that accepts a number and console.logs
+the multiplication table for that number (just multiply by the numbers 1 through 10)
+
+For example, showMultiplicationTable(7) should output
+
+7 x 1 = 7
+7 x 2 = 14
+7 x 3 = 21
+7 x 4 = 28
+7 x 5 = 35
+7 x 6 = 42
+7 x 7 = 49
+7 x 8 = 56
+7 x 9 = 63
+7 x 10 = 70
+ */
+function showMultiplicationTable(number) {
+    for (number = 1 ; number < 11 ; number++) {
+        var number2 = number * 7;
+        console.log("7 X " + number +" = " + number2 );
+    }
+}
+showMultiplicationTable();
+
+/* 3.
+Use a for loop and the code from the previous lessons to generate 10 random numbers between
+20 and 200 and output to the console whether each number is odd or even. For example:
+
+123 is odd
+80 is even
+24 is even
+199 is odd
+...
+ */
--- js/while.js
+++ js/while.js
@@ -1,6 +1,43 @@
+"use strict";
+
 // While loop exercise
+// #2
+//Create a while loop that uses console.log() to create the output shown below.
 var i = 2;
 while (i <= 65536) {
     console.log(i);
     i = i * 2;
 }
+
+//#3
+// An ice cream seller can't go home until she sells all of her cones. Write a JS program that generates a random
+// number between 50 and 100 representing the amount of cones to sell. Your code should generate numbers between 1 and 5,
+// simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones
+// sold to each person. This is how you get the random numbers.
+
+// The output should be similar to the following:
+/* 5 cones sold...  // if there are enough cones
+    Cannot sell you 6 cones I only have 3...  // If there are not enough cones
+    Yay! I sold them all! // If there are no more cones
+*/
+// // This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+// This expression will generate a random number between 1 and 5
+// Math.floor(Math.random() * 5) + 1;
+
+do {
+    // State how many cones I have to sell
+    console.log("I have " + allCones + " cone(s) to sell.");
+    // random variable coneSold from 1-5
+    var conesSold = Math.floor(Math.random() * 5) + 1;
+    if (conesSold <= allCones) {
+        allCones = allCones - conesSold;
+        console.log("I sold " + conesSold);
+    } else if (allCones === 0) {
+        console.log("Whoohoo! I sold all my cones!");
+    } else {
+        console.log("I can't sell you " + conesSold + " because I only have " + allCones + ".");
+    }
+} while(allCones !== 0);
+
