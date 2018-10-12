f64c9c9d79e6719d1eb1b7ff0c6d51cf0e88c541
benny-fields3rd <benny.fields3rd@gmail.com>
2018-10-12 11:55:16 -0500

    Completed math js exercise files

--- js/circle.js
+++ js/circle.js
@@ -8,17 +8,18 @@
         getArea: function () {
             // TODO: complete this method
             // hint: area = pi * radius^2
-
-            return; // TODO: return the proper value
+            return Math.PI * Math.pow(circle.radius, 2); // can use this as well instead of circle.
+            // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
-
+            var area = circle.getArea(); // can use this as well instead of circle.
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
-
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            if (doRounding) {
+                area = Math.round(circle.getArea()); // can use this as well instead of circle.
+            } console.log("Area of a circle with radius: " + this.radius + ", is: " + area);
         }
     };
 
@@ -30,7 +31,7 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+    circle.radius = 5;
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
