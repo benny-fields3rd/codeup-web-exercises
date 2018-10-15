2b6e917b090ecd54f21c995e0cc5ad6c014a9485
benny-fields3rd <benny.fields3rd@gmail.com>
2018-10-15 16:49:20 -0500

    cleaned up dom-query-js.html file and worked on dom-bonuses files

--- js/dom-bonuses2.js
+++ js/dom-bonuses2.js
@@ -1,1 +1,26 @@
 "use strict";
+// ================================= DOM EVENT BONUSES
+//
+// BONUS 1 - create a stop watch app
+//
+// 1) stop watch display that shows the time to the nearest 100th of a second
+// 2) start button to start the stop watch
+// 3) reset button to reset the time to zero
+// 4) pause button to stop the stop watch
+//
+// Additional Features:
+// 1) store a list of times in an aside div that are appended to every time the reset button is hit
+// 2) add delete buttons for each stored time that removes the time from the aside div
+// 3) add keyboard events to control starting, pausing, and resetting
+//
+// BONUS 2 - create a Hangman game
+//
+// Allow user 1 to enter a random word for user 2 to try to guess.
+//
+// - display the number of guesses
+// - display the letters already attempted
+// - display the word as it is being completed and the letters not already guessed with underscores
+// - a form should allow the user to type a letter guess
+// - do not accept inputs that are more than one letter or are not letters (ignore case)
+//
+// BONUS 3 - Create a zoomable font size effect with mouse scroll
