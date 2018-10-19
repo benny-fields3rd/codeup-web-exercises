e950e2c003cee50246a5cf01ac46cbc261ebb33b
benny-fields3rd <benny.fields3rd@gmail.com>
2018-10-19 16:01:08 -0500

    added jquery intro lessons

---
+++ js/jquery-intro-test.js
@@ -0,0 +1,27 @@
+"use strict";
+// Type the following right below the use strict or if not using use strict, the very top of the page.
+$(document).ready(function () {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); // closing of the $(document).ready(function() {
