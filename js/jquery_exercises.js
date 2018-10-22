380878173184b826b222b3e11603bee1b889b5d2
benny-fields3rd <benny.fields3rd@gmail.com>
2018-10-22 14:17:00 -0500

    completed mouse events exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
 // Update the jQuery code to select and alert a different id.
 // Use the same id on 2 elements. How does this change the jQuery selection?
 // var contents2 = $('#currentModels').html();
-    alert($('#intro').html());
+//     alert($('#intro').html());
 // alert(contents2); // it only shows the first instance of the id.
 
 // Class Selectors
@@ -26,7 +26,7 @@ $(document).ready(function () {
 
 // Element Selectors
 // Using jQuery, set the font-size of all li elements to 20px.
-$('li').css('font-size', '20px');
+// $('li').css('font-size', '20px');
 
 // Craft selectors that highlight all the h1, p, and li elements.
 // $('h1').css('background-color', 'yellow');
@@ -35,14 +35,31 @@ $('li').css('font-size', '20px');
 
 // Create a jQuery statement to alert the contents of your h1 element(s).
 // var h1Content = $('h1').html();
-alert(h1Content);
+// alert(h1Content);
 
 // Multiple Selectors
 //Combine your selectors that highlight all the h1, p, and li elements.
-$('h1, p, li').css('background-color', 'yellow');
+// $('h1, p, li').css('background-color', 'yellow');
 
+// Mouse event exercises
+//Remove your custom jQuery code from previous exercises.
 
-
+// Add jQuery code that will change the background color of a h1 element when clicked.
+$('h1').click(function () {
+   $(this).css('background-color', 'red');
+});
+// Make all paragraphs have a font size of 18px when they are double clicked.
+$('p').dblclick(function () {
+    $(this).css('font-size', '18px');
+});
+// Set all li text color to red when the mouse is hovering, reset to black when it
+// is not.
+$('li').hover(function () {
+    $(this).css('color', 'red');
+}, function () {
+    $(this).css('color', 'black');
+    }
+);
 
 
 }); // closing of the $(document).ready(function() {
