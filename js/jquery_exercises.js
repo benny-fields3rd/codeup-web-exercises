99d034fb1729d91ba042889c01aacf281b1401a7
benny-fields3rd <benny.fields3rd@gmail.com>
2018-10-22 14:07:11 -0500

    cleaned up jquery_exercises.html file and completed the selectors jquery exercise on jquery_exercises.js file

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -2,6 +2,9 @@
 
 // Type the following right below the use strict or if not using use strict, the very top of the page.
 $(document).ready(function () {
+    // a shortcut for jQuery is:
+    //$(function() {
+// });
 //     alert( 'The DOM has finished loading!' );
 
 // ID Selectors
@@ -12,6 +15,7 @@ $(document).ready(function () {
 // Update the jQuery code to select and alert a different id.
 // Use the same id on 2 elements. How does this change the jQuery selection?
 // var contents2 = $('#currentModels').html();
+    alert($('#intro').html());
 // alert(contents2); // it only shows the first instance of the id.
 
 // Class Selectors
@@ -25,9 +29,9 @@ $(document).ready(function () {
 $('li').css('font-size', '20px');
 
 // Craft selectors that highlight all the h1, p, and li elements.
-$('h1').css('background-color', 'yellow');
-$('p').css('background-color', 'yellow');
-$('li').css('background-color', 'yellow');
+// $('h1').css('background-color', 'yellow');
+// $('p').css('background-color', 'yellow');
+// $('li').css('background-color', 'yellow');
 
 // Create a jQuery statement to alert the contents of your h1 element(s).
 // var h1Content = $('h1').html();
@@ -36,16 +40,6 @@ alert(h1Content);
 // Multiple Selectors
 //Combine your selectors that highlight all the h1, p, and li elements.
 $('h1, p, li').css('background-color', 'yellow');
-
-
-
-
-
-
-
-
-
-
 
 
 
